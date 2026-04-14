049ceca6b0e4cd806be82d758cd579f74861f275
nrask1 <136827547+nrask1@users.noreply.github.com>
2026-04-14 19:21:16 -0400

    sifted through Banquet results for useful results

--- Main/HardwareScripts/visualize_testbed_results.py
+++ Main/HardwareScripts/visualize_testbed_results.py
@@ -19,11 +19,12 @@ def load_csv_to_arrays(csv_path):
     return arrays
 
 def main():
-    arrays = load_csv_to_arrays(os.path.join(PROJECT_ROOT, 'Main', 'HardwareScripts', 'bertha_imu_wheel_log.csv'))
+    # enter csv file path here to visualize
+    arrays = load_csv_to_arrays(os.path.join(PROJECT_ROOT, 'Main', 'HardwareScripts', 'results', 'multiple_spins_lag.csv'))
     for i in range(len(arrays)):
         # print(arrays[i])
         simulator.game_visualize(np.array([arrays[i]]), i)
-        sleep(0.1)
+        sleep(0.05)
 
 main()
 
